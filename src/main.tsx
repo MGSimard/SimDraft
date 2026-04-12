f664366320218e2be2be3ca4970dcc1a6129eed3
MGSimard <mgsimard.dev@gmail.com>
2026-04-12 02:53:34 -0400

    Add these because why not, going to check if there's anything new on tanstack's side

--- src/main.tsx
+++ src/main.tsx
@@ -7,21 +7,21 @@ import { PageNotFound } from "@/components/NotFound";
 import "@/css/global.css";
 import "@/css/fonts.css";
 
-// Create a new router instance
 const router = createRouter({
   routeTree,
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  defaultViewTransition: false,
   defaultNotFoundComponent: PageNotFound,
   defaultErrorComponent: PageError,
 });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
 
-// Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
